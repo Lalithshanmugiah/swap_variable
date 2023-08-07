7ce0871a313574eef0be644ca2a11dc9811beddc
Lalithshanmugiah <89589081+Lalithshanmugiah@users.noreply.github.com>
2023-08-07 15:13:02 +0530

    swap var

---
+++ script.js
@@ -0,0 +1,11 @@
+// swapping two variables
+ 
+let first = prompt('enter the fisrt variable ');
+let second = prompt('enter the secondvariable ');
+
+let temp = first;
+first = second;
+second = temp;
+
+console.log((first));
+console.log((second));
